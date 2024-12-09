be5f3b478641b42c6d60c702022777a16278119d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-09 21:10:01 +0530

    validation

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createTodo } = require("./types");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 
@@ -8,6 +8,7 @@ app.use(express.json());
 
 app.post("/todo", function (req, res) {
 
+    // validation
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload.success) {
@@ -18,13 +19,28 @@ app.post("/todo", function (req, res) {
     }
 
 
+
+
 })
 
 app.get("/todo", function (req, res) {
 
+
+
 })
 
 app.put("/completed", function (req, res) {
+
+    // validation
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "you sent the wrong inputs",
+        })
+        return;
+    }
 
 })
 
