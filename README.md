9bd2e80b90f7226121527891e57b487644a2723d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-09 23:47:13 +0530

    cors

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 npm i express
 npm i zod
 npm i mongoose
+npm i cors -> for silent requests from the frontend
 npm create vite@latest
 npm i - in frontend
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
-const { todo } = require("./db")
+const { todo } = require("./db");
+const cors = require("cors");
 const app = express();
 
 
