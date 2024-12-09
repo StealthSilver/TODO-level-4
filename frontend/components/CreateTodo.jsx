ae2dcdce88302e5ce9a087cefaadfb7497461117
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-09 23:59:05 +0530

    connect

--- frontend/components/CreateTodo.jsx
+++ frontend/components/CreateTodo.jsx
@@ -1,16 +1,37 @@
 // the static input boxes being exported
-
+import { useState } from "react"
 export function CreateTodo(){
+
+    const [title , setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
     return <div>
 
         <input style={{
             padding: 10, margin: 10
-        }} type="text" placeholder="title" /> <br />
+        }} type="text" placeholder="title" onChnage = {function(e) {
+            const value = e.target.value;
+        }} /> <br />
         <input  style={{
             padding: 10, margin: 10
-        }} type="text" placeholder="descripton" /> <br />
+        }} type="text" placeholder="descripton"  onChnage = {function(e) {
+            const value = e.target.value;
+        }}/> <br />
         <button style={{
             padding: 10, margin: 10
+        }} onClick={() => {
+            fetch("http://localhost:3000/todos" , {
+                method: "POST", body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "contentType": "application/json"
+                }
+            }).then(async function(res){
+                const json = await res.json();
+                alert("Todo Added");
+            })
         }}>Add a todo</button>
 
     </div>
