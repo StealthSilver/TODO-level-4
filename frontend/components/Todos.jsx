94d97ff5f18cd6bdf89b965fa0c03206ec7711de
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-09 23:41:25 +0530

    loop

--- frontend/components/Todos.jsx
+++ frontend/components/Todos.jsx
@@ -1,10 +1,16 @@
 
 // exporting the todos
-export function Todos(){
+export function Todos({todos}){
 
     return <div>
-        <h1>Go to gym</h1>
-        <h2>You need to go to the gym</h2>
-        <button>Mark as Complete</button>
+        {
+            todos.map(function(todo){
+                return <div>
+                    <h1>{todo.title}</h1>
+                    <h2>{todo.description}</h2>
+                    <button>{todo.completed == true ? "completed" : "Mark as complete"}</button>
+                </div>
+            })
+        }
     </div>
 }
